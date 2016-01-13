f7d2c652f42f2e225c3a082b9f817f6a090fb055
Steve Silvestri <steve.silvestri@digitalreasoning.com>
2016-01-13 17:08:25 -0500

    cleanup

--- src/main/java/com/variphy/challenge/comparator/BeerDateComparator.java
+++ src/main/java/com/variphy/challenge/comparator/BeerDateComparator.java
@@ -8,14 +8,6 @@ import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
-/**
- * Created by Steven Silvestri on 1/12/16.
- *
- * Notes:
- *
- * Not a fan of throwing exceptions here. Another approach would be to see which object caused the problem
- * and then shift that object to either the front or back of the collection.
- */
 public class BeerDateComparator implements Comparator<JsonObject> {
 
 	private final DateTimeFormatter dateFormat = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
